7c1887de855a8d2a36bd054ef67813dd51362937
v1shva <vishva.sudantha@gmail.com>
2017-04-07 12:02:22 +0530

    Interface redesigned

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/loginSignup.php', function(req, res, next) {
     res.render('loginSignup', { title: 'Mplay' });
 });
 
+router.get('/test.php', function(req, res, next) {
+    res.render('test', { title: 'Mplay' });
+});
+
 
 
 module.exports = router;
