bb7f3c8cbd03082b20a914e714e9c683c02c996b
v1shva <vishva.sudantha@gmail.com>
2017-05-18 10:35:00 +0530

    emotion change functionality for user

---
+++ public/javascripts/modules/player/emo-bar-playlist.component.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Vishva on 4/25/2017.
+ */
+angular.
+module('player').
+component('emoBar', {
+    templateUrl: 'player/emobar.php',
+    controller: function EmoController() {
+        this.mood= [];
+        this.mood["happy"] = true;
+        this.currentMoods = ["happy"];
+
+        this.handleClick = function handleClick(item){
+            var currentMood = item.attributes["name"].value;
+            var index = this.currentMoods.indexOf(currentMood);
+            if(index ===-1){ // check whether the current mood is already in the currentMoods arrray
+                this.currentMoods.push(currentMood); // if not value is pushed to array
+                this.mood[currentMood] = true;
+            }
+            else if(this.currentMoods.length>1){
+                this.mood[currentMood] = false;
+                this.currentMoods.splice(index, 1);
+            }
+            if(this.currentMoods.length>3){
+                var removeMood = this.currentMoods.shift();
+                this.mood[removeMood] = false;
+            }
+
+
+        }
+
+
+    }
+});
