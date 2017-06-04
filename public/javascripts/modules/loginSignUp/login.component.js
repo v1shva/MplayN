21e25cb251f1b4f7a3532372d765d87d2473a287
v1shva <vishva.sudantha@gmail.com>
2017-06-04 18:13:55 +0530

    removed old login module

---
+++ public/javascripts/modules/loginSignUp/login.component.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vishva on 4/8/2017.
+ */
+'use strict';
+angular.
+module('loginSignUp').
+component('login', {
+    templateUrl: '/components/login',
+    controller: function LoginController() {
+        this.testValue = "Hello";
+    }
+});
--- public/javascripts/modules/loginSignup/login-signup.module.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Vishva on 4/8/2017.
- */
-'use strict';
-
-// Define the `loginSignUp` module
-angular.module('loginSignup', []);
