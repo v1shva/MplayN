e050013df1a4cbcf69cdc88809c3a80f65ea8e95
v1shva <vishva.sudantha@gmail.com>
2017-06-06 13:35:28 +0530

    login sign up module

---
+++ public/javascripts/modules/core/auth/auth-details.service.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.
+module('core.auth').
+factory('Song', ['$resource',
+    function Song($resource) {
+        return {
+            getSongsByEmo : $resource('/api/song/byEmotion', {}, {
+                get: {
+                    method: 'GET',
+                    params: {emotion: 'in-love'},
+                    isArray: false
+                }
+
+            }),
+            getSongsByUserID : $resource('/api/song/byUser', {}, {
+                get: {
+                    method: 'GET',
+                    params: {userID: '123'},
+                    isArray: false
+                }
+            }),
+            addNewSong : $resource('/api/song/addNew', {}, {
+                post: {
+                    method: 'post',
+                    isArray: false
+                }
+            }),
+
+        }
+    }
+]);
